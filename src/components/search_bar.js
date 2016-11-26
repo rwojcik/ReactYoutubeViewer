a77fe47d54ad54a213781ce7eae807510e7fc9ca
Rafal Wojcik <rafalwojcik3@gmail.com>
2016-11-26 21:49:56 +0100

    Add webpack-dev-server and more JS love

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,12 +2,8 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={event => console.log(event.target.value)} />;
   }
 }
-
-// const SearchBar = () => {
-//     return <input /> ;
-// };
 
 export default SearchBar;
