db1a451683c1416402c8de50bfa247bb893a595c
Rafal Wojcik <rafalwojcik3@gmail.com>
2016-11-27 22:41:43 +0100

    Finish basic React

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: props.term || '' };
+  }
+
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div className="search-bar offset-sm-2 col-sm-8">
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search YouTube..."
+            value={this.state.term}
+            onChange={event => this.onInputChange(event.target.value)} />
+        </div>
+      </div>
+    );
   }
 }
 
